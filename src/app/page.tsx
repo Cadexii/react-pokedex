c4e8fc884e27320c0df0a2c3b6cf755216715007
Cadexii <cadexiipl@gmail.com>
2025-05-15 12:42:26 +0200

    create container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import Container from "./components/Container/Container";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 export default function Home() {
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
       <Sidebar />
-      <h1>React Pokédex</h1>
+      <Container>
+        <h1>React Pokédex</h1>
+      </Container>
     </div>
   );
 }
