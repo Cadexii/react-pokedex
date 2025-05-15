ec199fe1061c8110e2d18d836061abfcdcf96010
Cadexii <cadexiipl@gmail.com>
2025-05-15 11:12:12 +0200

    create sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Sidebar from "./components/Sidebar/Sidebar";
+
 export default function Home() {
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <Sidebar />
       <h1>React Pokédex</h1>
     </div>
   );
